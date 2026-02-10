aad33cf1aab0b3229fb4482e1e9d5cc7717f48e7
dharunprakashgt14 <dharunprakashgt14@gmail.com>
2026-02-10 22:59:20 +0530

    if else if

---
+++ demo.java
@@ -0,0 +1,13 @@
+public class demo{
+    public static void main(String[]args)
+    {
+        int x =8;
+        int y=17;
+        int z=6;
+
+        if(x>y&&x>z)
+            System.out.println(x);
+        else if(y>x && y>z)
+            System.out.println(y);
+    }
+}
