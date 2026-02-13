fb9c2af62a02787dcb7b4b7b093f0798641b0233
dharunprakashgt14 <dharunprakashgt14@gmail.com>
2026-02-13 10:18:27 +0530

    do_while_loop

---
+++ do_while.java
@@ -0,0 +1,12 @@
+public class while_loop{
+    public static void main(String[]args){
+        int i=1;
+        do
+        {
+            System.out.println("HI"+i);
+            i++;
+        }while(i<=5);
+        
+
+    }
+}
